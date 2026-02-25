f74950a004881b1907b95a3b5142c51685780d26
Coulloch Mathis <mathis.coulloch@etu.iut-tlse3.fr>
2026-02-25 11:44:50 +0100

    first

---
+++ src/config/configuration.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+// Configuration des variables pour l'acces bd
+return [
+    'base' => [
+        'server' => 'mysql-projetphp-michon-coulloch.alwaysdata.net:3306',
+        'db' => 'projetphp-michon-coulloch_bd',
+        'login' => '442040_user',
+        'mdp' => '$iutinfo',
+        'type' => 'mysql',
+    ],
+];
+
+
