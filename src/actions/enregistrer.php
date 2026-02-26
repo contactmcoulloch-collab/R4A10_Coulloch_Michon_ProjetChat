dd37e055a26bd26a0cfd85f5de9957db05fdca3a
Coulloch Mathis <mathis.coulloch@etu.iut-tlse3.fr>
2026-02-26 09:30:36 +0100

    debut sur enre et recup

--- src/actions/enregistrer.php
+++ src/actions/enregistrer.php
@@ -1,6 +1,8 @@
 <?php
 
-
-
+$req = $linkpdo->prepare('INSERT INTO MESSAGE (Id_User,content,horaire) VALUES (:iduser,:content,:horaire)');
+$req->execute(['iduser' => $idUser,
+"content" => $content,
+"horaire" => date("Y-m-d H:i:s")]);
 
 ?>
